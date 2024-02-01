ed9aaf97592ee20d12bf02320d3b2557646d2c1f
Vishnu <vishnu21012001@gmail.com>
2024-02-01 18:13:07 +1100

    Update

--- SIT725-2023-t2-prac7/Week-7/dbConnection.js
+++ SIT725-2023-t2-prac7/Week-7/dbConnection.js
@@ -1,12 +1,12 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:admin2@cluster0.l9hmwv8.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://s222357806:sharmaji222@vishnu.mmg1wjx.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 client.connect();
